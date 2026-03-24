4e5d2f3efd653da468019a474184ab2751c3e286
Roland Kindermann <kindermann.roland@gmail.com>
2026-03-24 20:34:29 +0100

    Re-worked noise

--- src/definition_ui.rs
+++ src/definition_ui.rs
@@ -2,10 +2,12 @@ use std::{fmt::Write as _};
 
 use egui::{Button, TextEdit, text::{CCursor, CCursorRange}};
 use strum::{EnumCount, IntoEnumIterator};
-use crate::{IMG_SIZE, RetroTexApp, color::{Color, EditableColor}, definition::{Coverage, TexturePass}, util::add_enum_dropdown};
+use crate::{IMG_SIZE, RetroTexApp, color::{Color, EditableColor}, definition::{NoiseType, TexturePass}, util::add_enum_dropdown};
 
 #[allow(unused_imports)]
 use log::{debug, error, log_enabled, info, warn, trace};
+
+const SECTION_SPACING: f32 = 10.0;
 
 pub enum PassOperation { Remove(usize), MoveUp(usize), MoveDown(usize) }
 
@@ -64,6 +66,24 @@ pub fn add_color_edit<const ALPHA: bool>(ui: &mut egui::Ui, editable: &mut Edita
         ui.ctx().request_repaint();
     }
 }
+
+fn mode_selector<T: IntoEnumIterator + AsRef<str> + Eq + EnumCount + Copy>(ui: &mut egui::Ui, val: &mut T, label: &str) -> Option<T> {
+    let mut ret = None;
+    ui.horizontal(| ui | {
+        ui.label(label);
+        let aw = ui.available_width();
+        let btn_w = (aw - ui.spacing().item_spacing.x * (T::COUNT as f32 - 1.0)) /  T::COUNT as f32;
+        for cur in T::iter() {
+            let btn = egui::Button::selectable(*val == cur, cur.as_ref());
+            if ui.add_sized([btn_w, ui.spacing().interact_size.y], btn).clicked() {
+                *val = cur;
+                ret = Some(cur);        }
+        }
+    });
+    ui.add_space(SECTION_SPACING);
+    ret
+}
+
 
 impl RetroTexApp {
     pub fn definition_ui(&mut self, ui: &mut egui::Ui) {
@@ -129,67 +149,51 @@ impl RetroTexApp {
 
                     ui.horizontal_wrapped(| ui | {
                         add_color_edit(ui, &mut pass.color, monospace_width);
-                        if pass.uses_both_colors() {
-                            add_color_edit(ui, &mut pass.color2, monospace_width);
-                        }
-                    });
-
-                    ui.horizontal_wrapped(| ui | { 
-                        ui.checkbox(&mut pass.perlin.enabled, "Perlin");
-                        if pass.perlin.enabled {
-                            ui.label("Scale:");
-                            ui.add(egui::DragValue::new(&mut pass.perlin.scale).range(1..=IMG_SIZE));
-                            ui.label("Octaves:");
-                            ui.add(egui::DragValue::new(&mut pass.perlin.octaves).range(1..=4));
-                            ui.checkbox(&mut pass.perlin.use_threshold, "Threshold");
-                            if pass.perlin.use_threshold {
-                                ui.add(egui::DragValue::new(&mut pass.perlin.threshold).range(0..=100));
-                            }
-                            reseed_button(ui, &mut pass.perlin.seed);
-                        }
-                    });
-                    
-                    ui.horizontal_wrapped(| ui | {
-                        ui.checkbox(&mut pass.white_noise.enabled, "White Noise");
-                        if pass.white_noise.enabled {
-                            ui.label("Scale:");
-                            ui.add(egui::DragValue::new(&mut pass.white_noise.scale).range(1..=(IMG_SIZE/2)));
-                            ui.checkbox(&mut pass.white_noise.use_threshold, "Threshold");
-                            if pass.white_noise.use_threshold {
-                                ui.add(egui::DragValue::new(&mut pass.white_noise.threshold).range(0..=100));
-                            }
-                            reseed_button(ui, &mut pass.white_noise.seed);
-                        }
-                    });
-
-                    if pass.perlin.enabled || pass.white_noise.enabled {
-                        ui.horizontal_wrapped(| ui | {
-                            ui.label("Noise Mode:");
-                            add_enum_dropdown(ui, &mut pass.noise_mode, "noise_mode", pass_idx, false);
-                        });
-                    }
-
-                    ui.horizontal( | ui | {
                         ui.label("Blend:");
                         add_enum_dropdown(ui, &mut pass.blend_mode, "blend_mode", pass_idx, false);
                     });
 
                     ui.separator();
-                    ui.horizontal(| ui | {
-                        ui.label("Shape:");
-                        let aw = ui.available_width();
-                        let btn_w = (aw - ui.spacing().item_spacing.x * (Coverage::COUNT as f32 - 1.0)) /  Coverage::COUNT as f32;
-                        for v in Coverage::iter() {
-                            let cov = v;
-                            let btn = egui::Button::selectable(pass.coverage == cov, v.as_ref());
-                            if ui.add_sized([btn_w, ui.spacing().interact_size.y], btn).clicked() {
-                                pass.coverage = cov;
-                                if cov.is_gizmo_editable() {
-                                    self.preview_editing = Some(pass_idx);
-                                }
+                    mode_selector(ui, &mut pass.noise.noise_type, "Noise");
+
+                    if pass.uses_noise() {
+                        ui.horizontal_wrapped(| ui | {
+                            ui.label("Noise Mode:");
+                            add_enum_dropdown(ui, &mut pass.noise.mode, "noise_mode", pass_idx, false);
+                            match pass.noise.noise_type {
+                                NoiseType::Perlin => {
+                                    ui.label("Strength:");
+                                    ui.add(egui::DragValue::new(&mut pass.noise.perlin_strength.v).range(0..=400));
+                                    ui.label("Scale:");
+                                    ui.add(egui::DragValue::new(&mut pass.noise.perlin_scale.v).range(1..=IMG_SIZE));
+                                    ui.label("Octaves:");
+                                    ui.add(egui::DragValue::new(&mut pass.noise.perlin_octaves).range(1..=4));
+                                },
+                                NoiseType::White | NoiseType::Gaussian => {
+                                    ui.label("STD:");
+                                    ui.add(egui::DragValue::new(&mut pass.noise.std.v).range(0..=100));
+                                    ui.label("Scale:");
+                                    ui.add(egui::DragValue::new(&mut pass.noise.pixel_scale).range(1..=IMG_SIZE/2));
+                                },
+                                NoiseType::None => {},
                             }
+                            ui.checkbox(&mut pass.noise.use_threshold, "Threshold");
+                            if pass.noise.use_threshold {
+                                ui.add(egui::DragValue::new(&mut pass.noise.threshold.v).range(0..=100));
+                            }
+                            reseed_button(ui, &mut pass.noise.seed);
+                        });
+                    }   
+
+                    ui.separator();
+                    let new_coverage = mode_selector(ui, &mut pass.coverage, "Shape:");
+                    if let Some(cov) = new_coverage {
+                        if cov.is_gizmo_editable() {
+                            self.preview_editing = Some(pass_idx);
+                        } else if self.preview_editing == Some(pass_idx) {
+                            self.preview_editing = None;
                         }
-                    });
+                    }
 
                     if pass.is_rect() {
                         ui.horizontal(| ui | {
@@ -199,7 +203,7 @@ impl RetroTexApp {
                             ui.add(egui::DragValue::new(&mut pass.rect.width).range(1..=(2 * IMG_SIZE - 1)).prefix("W:"));
                             ui.add(egui::DragValue::new(&mut pass.rect.height).range(1..=(2 * IMG_SIZE - 1)).prefix("H:"));
                             let is_editing = self.preview_editing == Some(pass_idx);
-                            let edit_button = egui::Button::selectable(is_editing, if is_editing { "Editing" } else { "Edit" });
+                            let edit_button = egui::Button::selectable(is_editing, if is_editing { "Editing rect..." } else { "Edit rect" });
                             if ui.add_sized([ui.available_width(), ui.spacing().interact_size.y], edit_button).clicked() {
                                 if is_editing {
                                     self.preview_editing = None;
@@ -217,45 +221,61 @@ impl RetroTexApp {
                             }
                         });
 
-                        ui.horizontal_wrapped(| ui | {
-                            ui.checkbox(&mut pass.rect.bevel.enabled, "Bevel");
+                        let mut bevel_enabled_width = 0.0;
+                        ui.horizontal(| ui | {
+                            let resp = ui.checkbox(&mut pass.rect.bevel.enabled, "Bevel");
+                            bevel_enabled_width = resp.rect.width();
                             if pass.rect.bevel.enabled {
                                 ui.add(egui::DragValue::new(&mut pass.rect.bevel.size).range(1..=IMG_SIZE));
                                 ui.label("Steepness:");
                                 ui.add(egui::DragValue::new(&mut pass.rect.bevel.steepness).range(-10..=10));
                                 ui.checkbox(&mut pass.rect.bevel.convex, "Convex");
+                            }
+                        });
+                        if pass.rect.bevel.enabled {
+                            ui.horizontal(| ui | {
+                                ui.add_space(bevel_enabled_width + ui.spacing().item_spacing.x);
                                 ui.label("Ease:");
                                 ui.checkbox(&mut pass.rect.bevel.ease_in, "In");
                                 ui.checkbox(&mut pass.rect.bevel.ease_out, "Out");
-                            }
-                        });
+                            });
+                            ui.add_space(SECTION_SPACING);
+                        }
 
-                        ui.horizontal_wrapped(| ui | {
-                            ui.checkbox(&mut pass.tile.enabled, "Tile");
+                        let mut tile_enabled_width = 0.0;
+                        ui.horizontal(| ui | {
+                            let resp = ui.checkbox(&mut pass.tile.enabled, "Tile");
+                            tile_enabled_width = resp.rect.width();
                             if pass.tile.enabled {
                                 ui.label("Gap:");
-                                ui.add(egui::DragValue::new(&mut pass.tile.x_gap).range(2..=IMG_SIZE));
-                                ui.add(egui::DragValue::new(&mut pass.tile.y_gap).range(2..=IMG_SIZE));
+                                ui.add(egui::DragValue::new(&mut pass.tile.x_gap).range(0..=IMG_SIZE-2).prefix("X:"));
+                                ui.add(egui::DragValue::new(&mut pass.tile.y_gap).range(0..=IMG_SIZE-2).prefix("Y:"));
                                 ui.label("Count:");
-                                ui.add(egui::DragValue::new(&mut pass.tile.x_count).range(1..=IMG_SIZE/2));
-                                ui.add(egui::DragValue::new(&mut pass.tile.y_count).range(1..=IMG_SIZE/2));
+                                ui.add(egui::DragValue::new(&mut pass.tile.x_count).range(1..=IMG_SIZE/2).prefix("X:"));
+                                ui.add(egui::DragValue::new(&mut pass.tile.y_count).range(1..=IMG_SIZE/2).prefix("Y:"));
+                            }
+                        });
+                        if pass.tile.enabled {
+                            ui.horizontal(| ui | {
+                                ui.add_space(tile_enabled_width + ui.spacing().item_spacing.x);
                                 ui.label("Shift:");
-                                ui.add(egui::DragValue::new(&mut pass.tile.shift).range(-IMG_SIZE/2..=IMG_SIZE/2));
+                                ui.add(egui::DragValue::new(&mut pass.tile.shift).range(-IMG_SIZE/2..=IMG_SIZE/2).prefix(if pass.tile.shift_direction == crate::definition::TileShiftDirection::Horizontal { "X:" } else { "Y:" }));
                                 if pass.tile.shift > 0 {
                                     ui.label("Direction:");
                                     add_enum_dropdown(ui, &mut pass.tile.shift_direction, "tile_shift_direction", pass_idx, false);
                                 }
-                            }
-                        });
-                        if pass.tile.enabled {
-                            ui.horizontal_wrapped(| ui | {
+                            });
+
+                            ui.horizontal(| ui | {
+                                ui.add_space(tile_enabled_width + ui.spacing().item_spacing.x);
                                 ui.checkbox(&mut pass.tile.variation_enabled, "Variation");
                                 if pass.tile.variation_enabled {
                                     ui.label("Strength:");
-                                    ui.add(egui::DragValue::new(&mut pass.tile.variation).range(1..=400));
+                                    ui.add(egui::DragValue::new(&mut pass.tile.variation.v).range(1..=400));
                                     reseed_button(ui, &mut pass.tile.variation_seed);
                                 }
                             });
+                            ui.add_space(SECTION_SPACING);
                         };
                     }
 
